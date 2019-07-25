cd041ed81dbed045d45149322f4cf5d87906e8c0
baryan <34631415+baryan@users.noreply.github.com>
2019-07-25 15:36:02 -0700

    Added IR 2m sensor and autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/section1/BasicTestLeagueThing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/section1/BasicTestLeagueThing.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.section1;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -36,13 +37,16 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.SensorDigitalTouch;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "Basic: League Test Drive", group = "Iterative Opmode")
+@Disabled
 public class BasicTestLeagueThing extends OpMode {
 
     // Declare OpMode members.
@@ -53,6 +57,7 @@ public class BasicTestLeagueThing extends OpMode {
 
     private Servo clawServo = null;
     private CRServo armMotor = null;
+    private DistanceSensor sensorRange = null;
 
     private DigitalChannel armStop = null;
 
@@ -84,6 +89,9 @@ public class BasicTestLeagueThing extends OpMode {
         // arm touch sensor
         armStop = hardwareMap.get(DigitalChannel.class, "armStop");
         armStop.setMode(DigitalChannel.Mode.INPUT);
+
+        // REV Robotics IR 2m Sensor
+        sensorRange = hardwareMap.get(DistanceSensor.class, "sensor_range");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -134,6 +142,12 @@ public class BasicTestLeagueThing extends OpMode {
         // Setup a variable for each side of the robot
         double leftPower;
         double rightPower;
+        double targetRange;
+        final double MAX_RANGE = 36.0;
+        final double MID_SPEED = 0.25;
+        final double MID_RANGE = 12.0;
+        final double MIN_SPEED = 0.125;
+        final double MIN_RANGE = 1.0;
 
         // This driving mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
@@ -151,6 +165,22 @@ public class BasicTestLeagueThing extends OpMode {
         double driveY = -gamepad1.left_stick_y;
         double driveX = gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
+
+        // Get the range to target using the distance sensor.  Allow movement forward only if there
+        // is no obstacles with 1ft of the robot
+        targetRange = sensorRange.getDistance(DistanceUnit.INCH);
+        telemetry.addData("Distance to obstacle: ", "%4.2f", targetRange);
+        if (driveY > 0){
+            if (targetRange <= MIN_RANGE){
+                driveY = 0;
+            }
+            else if (targetRange <= MID_RANGE){
+                driveY = MIN_SPEED;
+            }
+            else if (targetRange <= MAX_RANGE){
+                driveY = MID_SPEED;
+            }
+        }
 
         // ^ condensed
         double leftFrontPower = Range.clip((driveY + driveX) + turn, -1.0, 1.0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/section4/AutonomousOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/section4/AutonomousOpmode.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode.section4;
 
-public class AutonomousOpmode {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Basic: Autonomous", group="Linear Opmode")
+public class AutonomousOpmode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Put initialization code between here and "waitforStart()"
+
+        waitForStart();
+
+        // Put the autonomous portion of the code here
+
+    }
+
 }
